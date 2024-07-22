e9be4bca588e654e4fe6460aaf6c152ea793a5bf
CarlRocha <carlos.rocha@fullbar.com.br>
2024-07-22 01:35:19 -0300

    fix/ add db.json

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,16 @@
-const jsonServer = require("json-server");
 const express = require("express");
 const cors = require("cors");
 const removePassword = require("./middlewares/removePassword");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 
-const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults();
+const server = express();
 
 server.use(express.json());
 server.use(cors());
 server.use(removePassword);
 server.use("/auth", authRoutes);
 server.use("/users", userRoutes);
-server.use(middlewares);
-server.use(router);
 
 server.listen(process.env.PORT || 3001, () => {
   console.log("JSON Server is running on port 3001");
